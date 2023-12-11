38a43b6b57c303d2abde107f3db128304ab53009
ThakurGopal27 <gopals98877@gmail.com>
2023-12-11 11:22:40 +0530

    first

---
+++ src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key":
+            process.env.REACT_APP_YOUTUBE_API_KEY || "86d9191733msh6d8d56619804e5ap14109ejsn30ac99befa6b",
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
